21dca804a0249605744adf60393cd6d51dd07ad6
Jack Moseley <jack.moseley@vodafone.com>
2018-09-02 20:47:27 +0100

    Initial Commit

---
+++ golfstats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  golfstats
+//
+//  Created by Jack Moseley on 02/09/2018.
+//  Copyright © 2018 mystats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
